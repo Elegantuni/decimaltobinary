aa44662759772ce71aef885eb72271b7603d4e7c
Anthony Brown <groundup2360917182914017@gmail.com>
2019-02-11 18:52:30 -0500

    Add better examples

--- example.cpp
+++ example.cpp
@@ -1,21 +1,57 @@
 #include <iostream>
 #include "dectobin.h"
+#include <unistd.h>
 
 using namespace std;
 
+void printdec(int *data1, int data2);
+
 int main(int argc, char *argv[])
 {
-	int buffer1[5];
+	int number1 = 1;
+	int number2 = 6;
+	int result1;
+	
+	const int thedatasize = 8;
+	int buffer1[thedatasize];
+	int buffer2[thedatasize];
+	int buffer3[thedatasize];
+	
+	dectobin(number1, buffer1, thedatasize);
+	dectobin(number2, buffer2, thedatasize);
 
-	dectobin(-2, buffer1, 5);
+	cout << number1 << " in binary is ";
 
-	for(int i = 0; i < 5; i++)
-	{
-		cout << buffer1[i];
-	}
+	printdec(buffer1, thedatasize);
+	
+	cout << endl;
 
+	cout << number2 << " in binary is ";
+
+	printdec(buffer2, thedatasize);
+
+	result1 = number1 | number2;
+
+	cout << endl;
+	
+	cout << number1 << " or " << number2 << " is " << result1 << endl;
+
+	cout << result1 << " in binary is ";
+
+	dectobin(result1, buffer3, thedatasize);
+
+	printdec(buffer3, thedatasize);
+	
 	cout << endl;
 
 	return 0;
+}
+
+void printdec(int *data1, int data2)
+{
+	for(int i = 0; i < data2; i++)
+	{
+		cout << data1[i];
+	}
 }
 
--- gmpexample.cpp
+++ gmpexample.cpp
@@ -3,12 +3,14 @@
 #include <stdlib.h>
 #include "gmpdectobin.h"
 
+void printgmpbin(mpz_t *data1, int data2);
+
 int main(int argc, char *argv[])
 {
-	int number1 = -1;
+	int number1 = 5;
 	mpz_t number2;
 	mpz_init(number2);
-	mpz_set_si(number2, -1);
+	mpz_set_si(number2, number1);
 	const int maxsize = 64;
 
 	mpz_t *buffer1 = (mpz_t *)malloc(sizeof(mpz_t) * maxsize);
@@ -22,13 +24,18 @@ int main(int argc, char *argv[])
 	
 	gmpdectobin(number2, buffer1, maxsize);
 
-	for(int i = 0; i < maxsize; i++)
-	{
-		gmp_printf("%Zd", buffer1[i]);
-	}
+	printgmpbin(buffer1, maxsize);
 
 	printf("\n");
 
 	return 0;
+}
+
+void printgmpbin(mpz_t *data1, int data2)
+{
+	for(int i = 0; i < data2; i++)
+	{
+		gmp_printf("%Zd", data1[i]);
+	}
 }
 
